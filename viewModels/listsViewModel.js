0f1c98653e1a0cc59770e634e4572f0d32122953
Pablo Arocha <pablo@media8.com>
2013-12-06 10:25:21 -0500

    temp

--- viewModels/listsViewModel.js
+++ viewModels/listsViewModel.js
@@ -4,15 +4,30 @@ function ListsViewModel() {
     self.lists = ko.observableArray(['Mi Lista 1', 'Mi Lista 2', 'Mi Lista 3', 'Mi Lista 5']);
    
 	//TODO: How to get the user selected country languague code to emebed in URL    
-    self.getListItemsUrl = 'http://m8staging.com/es-es/desktopmodules/AuthServices/API/PassPort.ashx/GetListItems';
-    
-    self.defaultParams = {
-	    country: 'uk',
-	    listing_type: 'buy',
-	
-	    // Used to signify we are using jsonp
-	    callback: '?'
-	};
+    //self.getListItemsUrl = 'http://m8staging.com/es-es/desktopmodules/AuthServices/API/PassPort.ashx/GetListItems';   
+
+	var txtPortalLang = 'es-es';
+
+	$$.ajax({
+	    type: 'GET', 
+	    url: 'http://m8staging.com/'+txtPortalLang+'/desktopmodules/AuthServices/API/PassPort.ashx/GetListItems',
+	    //data: {name: 'test250', pass: 'testtest', portal: '6'},
+	    dataType: 'xml', 
+	    async: true,
+	    success: function(response) {
+	    
+	    /*	if (response) {
+			   	Lungo.Notification.error("Error","Login Info Incorrect.  Please try again.", "cancel", 3);
+			} else {					
+				Lungo.Notification.success("Success","UID: "+response.uid, "check", 3, goHome);
+			}; 
+		*/				
+	    },
+	    error: function(xhr, type) { 
+	        Lungo.Notification.error("Error","Error retrieving lists.", "cancel", 3);
+	    }
+	});
+
 
     
 };
