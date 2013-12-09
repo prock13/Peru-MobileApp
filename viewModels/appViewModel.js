985b2adc8e416685d6c6f3ede0480d2f786a1308
Pablo Arocha <pablo@media8.com>
2013-12-09 16:39:47 -0500

    ListView Changes

--- viewModels/appViewModel.js
+++ viewModels/appViewModel.js
@@ -1,9 +1,9 @@
 function AppViewModel() {
     var self = this;
-
-	// LISTS  ////////////////////////////////////
-    //self.lists = ko.observableArray(['Mi Lista 1', 'Mi Lista 2', 'Mi Lista 3', 'Mi Lista 5']);
-    self.lists = ko.observableArray();
+	var txtUserName = 'test250';
+    var txtPassword = 'testtest';
+    var txtPortalLang  = 'es-es';
+    var listResults = '';
 
 /*	var cachedUserInfo = Lungo.Cache.get("lungoUserInfo");
 	var txtUserName = cachedUserInfo['userName'];
@@ -12,13 +12,11 @@ function AppViewModel() {
 	var txtPortalLang = cachedUserInfo['portalLang'];
 */
 
-	self.getListItems = function () {
-		//PMA - TESTING WITH HARDCODED DATA!!
-		var txtUserName = 'test250';
-	    var txtPassword = 'testtest';
-	    var txtPortalLang  = 'es-es';
-	    var name = '';	
-	
+
+	// LISTS  ////////////////////////////////////
+    self.listDetail = ko.observableArray();
+
+	self.getListItems = function() {
 		jQuery.ajax({
 	        type: "GET",
 	        url: "http://m8staging.com/"+txtPortalLang+"/desktopmodules/AuthServices/API/PassPort.ashx/GetListItems",
@@ -27,29 +25,32 @@ function AppViewModel() {
 	        cache: false,
 	        dataType: "xml",
 	        success: function(xml) {
+	        	listResults = xml;
+	        	var nid = ''; 
+	        	var name = '';
 	            $(xml).find('list').each(function(){
-	                name = $(this).find("listname").text()
-					self.lists.push(name);
+	                listNid = $(this).find("nid").text();
+	                listName = $(this).find("listname").text();
+					self.listDetail.push({nid: listNid , listname: listName});					
 	            });
 	        },
 	        error: function(xhr, type) { 
-	                Lungo.Notification.error("Error","Login Error.  Please try again.", "cancel", 3);
+	                Lungo.Notification.error("Error","Error retrieving lists.  Please try again.", "cancel", 3);
 	        }            
 	    });
 	};
 
+	// LIST DETAILS  ////////////////////////////////////
+	self.getListDetails	 = function() {
+		alert("It's working!");	
+	};
 
 	// BADGES  ////////////////////////////////////
     self.userBadgesTitle = 'Mis Sellos';
     self.userBadgesList = ko.observableArray();
     self.allBadgesList = ko.observableArray();
-    
-	self.getBadgeItems = function () {
-		//PMA - TESTING WITH HARDCODED DATA!!
-		var txtUserName = 'test250';
-	    var txtPassword = 'testtest';
-	    var txtPortalLang  = 'es-es';
-	
+
+	self.getBadgeItems = function() {
 		jQuery.ajax({
 	        type: "GET",
 	        url: "http://m8staging.com/"+txtPortalLang+"/desktopmodules/AuthServices/API/PassPort.ashx/GetBadgeItems",
@@ -59,28 +60,28 @@ function AppViewModel() {
 	        dataType: "xml",
 	        success: function(xml) {
 	            //User Badges
+				var userBadgeURL = '';
 	            $(xml).find('earnedbadges').each(function() {
 	            	$(this).find("badge").each(function() {
-	                	var userBadgeURL = $(this).text();
+	                	userBadgeURL = $(this).text();
 	                	self.userBadgesList.push(userBadgeURL);
 					});
 	            });
 	            
 				//All Badges
+				var allBadgeURL = '';
 	            $(xml).find('allbadges').each(function() {
 	            	$(this).find("badge").each(function() {
-	                	var allBadgeURL = $(this).text();
+	                	allBadgeURL = $(this).text();
 	                	self.allBadgesList.push(allBadgeURL);
 					});
 	            });	            
 	        },
 	        error: function(xhr, type) { 
-	                Lungo.Notification.error("Error","Error retrieving badges.", "cancel", 3);
+	                Lungo.Notification.error("Error","Error retrieving badges.  Please try again.", "cancel", 3);
 	        }            
 	    });
 	};
-    
-
     
 };
 
